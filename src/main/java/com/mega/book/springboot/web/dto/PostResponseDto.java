2ee82b192349a6df3e45d98c3e4ccb2fdf108068
JeMinYoo <simple6270@naver.com>
2022-10-04 11:05:43 +0900

    AWS 2일차

---
+++ src/main/java/com/mega/book/springboot/web/dto/PostResponseDto.java
@@ -0,0 +1,18 @@
+package com.mega.book.springboot.web.dto;
+
+import com.mega.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+    public PostResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
